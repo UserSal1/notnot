dc7bbc2e5c543666e1673e09e46a7bc6deddbad3
UserSal1 <149400297+UserSal1@users.noreply.github.com>
2025-03-25 18:56:28 +0000

    Redbull

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -1,90 +1,100 @@
-class HelloWorld {
-    getInfo() {
-      return {
-        id: 'helloworld',
-        name: 'JS+',
-        color1: '#FF8C1A',
-        color2: '#DB6E00',
-        blocks: [
-          {
-            opcode: 'hello',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'print and format [TEXT] with CSS style [CSS]',
-            arguments: {
-                TEXT: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: "Hello, World!"
-                },
-                CSS: {
-                    type: Scratch.ArgumentType.STRING,
-                    defaultValue: "color:yellow; font-size:50px;"
-                }
-            },
-            category: 'My Category'
-          },
-          {
-            opcode: 'colorget',
-            blockType: Scratch.BlockType.REPORTER,
-            text: "get [COLOR] color as CSS stroke",
-            arguments: {
-                COLOR: {
-                    type: Scratch.ArgumentType.COLOR
-                }
-            },
-            category: "CSS"
-          },
-          {
-            opcode: 'sizeget',
-            blockType: Scratch.BlockType.REPORTER,
-            text: "get [FONTSIZE] font size as CSS stroke",
-            arguments: {
-                FONTSIZE: {
-                    type: Scratch.ArgumentType.NUMBER,
-                    defaultValue: 24
-                }
-            },
-            category: "CSS"
-          },
-          {
-            opcode: 'random',
-            blockType: Scratch.BlockType.REPORTER,
-            text: "return random number",
-            category: 'My Category'
-          },
-          {
-            opcode: "webalert",
-            blockType: Scratch.BlockType.COMMAND,
-            text: "alert [MSG]",
-            arguments: {
-                MSG: {
-                    type: Scratch.ArgumentType.STRING
-                }
-            },
-            category: 'My Category'
-          }
-        ],
-      };
-    }
-  
-    hello(args) {
-      console.log(`%c${args.TEXT}`, `${args.CSS}`);
+const BlockType = require('../../extension-support/block-type');
+const ArgumentType = require('../../extension-support/argument-type');
+const TargetType = require('../../extension-support/target-type');
+
+class Scratch3YourExtension {
+
+    constructor (runtime) {
+        // put any setup for your extension here
     }
 
-    colorget(args) {
-        return `color:${args.COLOR}; `
+    /**
+     * Returns the metadata about your extension.
+     */
+    getInfo () {
+        return {
+            // unique ID for your extension
+            id: 'yourScratchExtension',
+
+            // name that will be displayed in the Scratch UI
+            name: 'Demo',
+
+            // colours to use for your extension blocks
+            color1: '#000099',
+            color2: '#660066',
+
+            // icons to display
+            blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
+            menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
+
+            // your Scratch blocks
+            blocks: [
+                {
+                    // name of the function where your block code lives
+                    opcode: 'myFirstBlock',
+
+                    // type of block - choose from:
+                    //   BlockType.REPORTER - returns a value, like "direction"
+                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
+                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
+                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
+                    blockType: BlockType.REPORTER,
+
+                    // label to display on the block
+                    text: 'My first block [MY_NUMBER] and [MY_STRING]',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // where this block should be available for code - choose from:
+                    //   TargetType.SPRITE - for code in sprites
+                    //   TargetType.STAGE  - for code on the stage / backdrop
+                    // remove one of these if this block doesn't apply to both
+                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+
+                    // arguments used in the block
+                    arguments: {
+                        MY_NUMBER: {
+                            // default value before the user sets something
+                            defaultValue: 123,
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.NUMBER
+                        },
+                        MY_STRING: {
+                            // default value before the user sets something
+                            defaultValue: 'hello',
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.STRING
+                        }
+                    }
+                }
+            ]
+        };
     }
 
-    sizeget(args) {
-        return `font-size:${args.FONTSIZE}px; `
-    }
 
-    random() {
-        return Math.random()
-    }
-
-    webalert(args) {
-        alert(args.MSG)
+    /**
+     * implementation of the block with the opcode that matches this name
+     *  this will be called when the block is used
+     */
+    myFirstBlock ({ MY_NUMBER, MY_STRING }) {
+        // example implementation to return a string
+        return MY_STRING + ' : doubled would be ' + (MY_NUMBER * 2);
     }
 }
-  
-Scratch.extensions.register(new HelloWorld());
+
+module.exports = Scratch3YourExtension;
